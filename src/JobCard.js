1521385a8951e830336cd8fa8f70f13e5b9c6125
Maria Juravic <mariajuravic@yahoo.com>
2023-01-25 15:17:17 -0800

    fixing code review todos and doc strings

--- src/JobCard.js
+++ src/JobCard.js
@@ -1,13 +1,21 @@
 import "./JobCard.css";
+import convertAndFormat from "./_helper"
 
-//TODO: not all jobs have equity  and sexify salary(proper salary)
+/**
+ *Simple presentation component for Job
+ *
+ * Props:
+ * -id, title, salary, company, equity
+ *
+ * {JobListCard} => JobCard
+ */
 function JobCard({ id, title, salary, company, equity }) {
   return (
     <div className="JobCard" id={id}>
       <h3>Title: {title}</h3>
       <h4>Company: {company}</h4>
-      <p>Salary: {salary}</p>
-      <p>Equity: {equity}</p>
+      <p>Salary: ${convertAndFormat(salary)}</p>
+      {equity && <p>Equity: {equity}</p>}
     </div>
   );
 }
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,18 +1,32 @@
 import { useState } from "react";
 import "./SearchForm.css";
 
+
+/**
+ *Search Form for both jobs and companies
+ *
+ * Props:
+ * -handleSearch()
+ *
+ * State:
+ * -formData
+ *
+ * {CompanyList or JobList} => SearchForm
+ */
 function SearchForm({ handleSearch }) {
 
   const [formData, setFormData] = useState({
     searchTerm: ""
   });
 
+  /** Update form input. */
   function handleChange(evt) {
     setFormData(fData => ({
       searchTerm: evt.target.value
     }));
   }
 
+  /** Call parent function and clear form. */
   function handleSubmit(evt) {
     evt.preventDefault()
     handleSearch(formData.searchTerm.trim())

694a48c10881c174f677256e1f561716823d9364
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-01-26 10:10:32 -0800

    WIP authentication

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
 import { BrowserRouter } from 'react-router-dom';
+import userContext from "./userContext";
 import './App.css';
 import Header from './Header';
 import RouteList from './RouteList';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const DEFAULT_USER_INFO = {
+  username: null,
+  email: null,
+  firstName: null,
+  lastName: null
+};
+
+const BASE_URL = "http://localhost:3001";
 
 /** Renders application */
 
 function App() {
+
+  function setToken() {
+
+  }
+
+  function getToken() {
+    return localStorage.getItem("token");
+  }
+
+  async function fetchUserDataFromAPI() {
+    /**
+     * TODO:
+     * - call getToken()
+     *  - store the full token into variable
+     *  - split token to get the payload
+     * - put payload into atob
+     * - destructure username
+     *
+     */
+    const resp = await axios.get(`${BASE_URL}/users/${username}`);
+  }
+
+  useEffect(function fetchAndSetUserInfo() {
+    async function fetchUser() {
+      const token = getToken();
+      if (token) {
+        console.log("token is present, fetch user info");
+      } else {
+        // TODO: remove
+        console.log("DEBUG: no user token found");
+      }
+    }
+    fetchUser();
+  }, []);
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <RouteList />
-      </BrowserRouter>
-    </div>
+    <userContext.Provider value={DEFAULT_USER_INFO}>
+      <div className="App">
+        <BrowserRouter>
+          <Header />
+          <RouteList />
+        </BrowserRouter>
+      </div>
+    </userContext.Provider>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,29 @@
 import { NavLink } from "react-router-dom";
 import "./Header.css";
+import userContext from "./userContext";
+import { useContext } from "react";
 
 /** Renders the header and navigation */
 
 function Header() {
+  const { username } = useContext(userContext);
   return (
     <header className="Header">
       <nav>
         <NavLink to="/">Jobly</NavLink>
         <div>
-          <NavLink to="/companies">Companies</NavLink>
-          <NavLink to="/jobs">Jobs</NavLink>
+          {username && <>
+            <NavLink to="/companies">Companies</NavLink>
+            <NavLink to="/jobs">Jobs</NavLink>
+            <NavLink to="/profile">Profile</NavLink>
+            <NavLink to="/profile">Log out, {username}.</NavLink>
+          </>
+          }
+          {!username && <>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/signup">Sign up</NavLink>
+          </>
+          }
         </div>
       </nav>
     </header>

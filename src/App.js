99d4e63726f798f36ef3dff0481c0db105fa396f
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-01-26 11:08:00 -0800

    Switch

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './Header';
 import RouteList from './RouteList';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import NotFound from './NotFound';
+import Loader from './Loader';
 
 const DEFAULT_USER_INFO = {
   username: null,
@@ -30,41 +32,34 @@ function App() {
   }
 
   async function fetchUserDataFromAPI() {
-    /**
-     * TODO:
-     *call getToken()
-     *  - store the full token into variable
-     *  - split token to get the payload
-     * - put payload into atob
-     * - destructure username
-     *
-     */
-
     const token = getToken();
     const payload = JSON.parse(atob(token.split(".")[1]));
     const { username } = payload;
 
     if (user.isLoading) {
-      const resp = await axios.get(`${BASE_URL}/users/${username}`,
-        {
-          headers: {
-            Authorization: token
-          }
-        });
-      resp.data.user.isLoading = false;
-      setUser(resp.data.user);
+      try {
+        const resp = await axios.get(`${BASE_URL}/users/${username}`,
+          {
+            headers: {
+              Authorization: token
+            }
+          });
+        resp.data.user.isLoading = false;
+        setUser(resp.data.user);
+      } catch (err) {
+        console.log(err);
+        // return <NotFound message="Problems fetching user..." />
+        return <h2>Problems fetching user...</h2>;
+      }
     }
   }
-
-  console.log("user=", user);
-
-  fetchUserDataFromAPI();
 
   useEffect(function fetchAndSetUserInfo() {
     async function fetchUser() {
       const token = getToken();
       if (token) {
         console.log("token is present, fetch user info");
+        fetchUserDataFromAPI();
       } else {
         // TODO: remove
         console.log("DEBUG: no user token found");
@@ -73,8 +68,13 @@ function App() {
     fetchUser();
   }, []);
 
+  // TODO: Ask if we should check presence of token or user.username
+
+  if (user.isLoading && getToken()) return <Loader />;
+
+
   return (
-    <userContext.Provider value={DEFAULT_USER_INFO}>
+    <userContext.Provider value={user}>
       <div className="App">
         <BrowserRouter>
           <Header />
--- src/Homepage.js
+++ src/Homepage.js
@@ -2,12 +2,22 @@
  * Renders the homepage
  *
  */
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import userContext from "./userContext";
 
 function Homepage() {
+  const { username } = useContext(userContext);
+
   return (
     <>
       <h1>Jobly!</h1>
       <p>All the jobs in one, convenient place.</p>
+      {username && <h2>Welcome Back, {username}!</h2>}
+      {!username && <>
+        <Link to="/login">Login</Link> <br /><br />
+        <Link to="/signup">Sign up</Link>
+      </>}
     </>
   );
 }

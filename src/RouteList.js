09c405f5bfdd0d16d3877402911c92bb051d7667
Maria Juravic <mariajuravic@yahoo.com>
2023-01-27 09:54:07 -0800

    added route protection

--- src/RouteList.js
+++ src/RouteList.js
@@ -6,20 +6,27 @@ import CompanyDetail from "./CompanyDetail";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm"
 import ProfileForm from "./ProfileForm";
+import userContext from "./userContext"
+import { useContext } from "react";
 
 /** Renders application routes */
 
 function RouteList({handleLogin, handleRegister}) {
+  const { user } = useContext(userContext);
+
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/login" element={<LoginForm handleLogin={handleLogin}/>} />
+      <Route path="/register" element={<RegisterForm handleRegister={handleRegister}/>} />
+
+      {user && <>
       <Route path="/companies" element={<CompanyList />} />
       <Route path="/companies/:handle" element={<CompanyDetail />} />
       <Route path="/jobs" element={<JobList />} />
-      <Route path="/login" element={<LoginForm handleLogin={handleLogin}/>} />
-      <Route path="/register" element={<RegisterForm handleRegister={handleRegister}/>} />
       <Route path="/profile" element={<ProfileForm />} />
-
+      </>
+      }
       {/* <Route path="/logout"/> */}
 
       <Route path="*" element={<Navigate to="/" />} />
